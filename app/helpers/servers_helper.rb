c325bdb30423b10311611e2b5afc89246260eff8
Bert Driehuis <bert.driehuis@cyso.nl>
2015-07-16 18:29:45 +0200

    Merge formatting update from master

--- app/helpers/servers_helper.rb
+++ app/helpers/servers_helper.rb
@@ -1,7 +1,8 @@
 module ServersHelper
   def display_retention(server)
     if server.retention_days.to_i > 0
-      "#{server.retention_days.to_i} daily, #{server.retention_weeks.to_i} weekly, #{server.retention_months.to_i} monthly"
+      days = server.retention_days.to_i + server.retention_weeks.to_i * 7 + server.retention_months.to_i * 31
+      "#{server.retention_days.to_i} daily, #{server.retention_weeks.to_i} weekly, #{server.retention_months.to_i} monthly (#{days} days total)"
     else
       server.keep_snapshots.to_s
     end
