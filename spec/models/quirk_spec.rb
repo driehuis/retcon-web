f7c53b5910e9fa53cc85f75018772fac26bd79f9
Bert Driehuis <bert.driehuis@cyso.nl>
2015-09-25 15:11:32 +0200

    Add validates_presence_of :comment top QuirkDetail; associated model tests.

--- spec/models/quirk_spec.rb
+++ spec/models/quirk_spec.rb
@@ -19,4 +19,22 @@ describe Quirk do
     p = Factory.build(:quirk, :description => nil)
     p.valid?.should be false
   end
+
+  it "should generate a valid QuirkDetail when associated with a Server" do
+    q = Factory.build(:quirk)
+    s = Factory.build(:server)
+    qd = QuirkDetail.new(:quirk_id=>q.id, :server_id=>s.id, :comment => '#12345')
+    q.valid?.should be true
+    s.valid?.should be true
+    qd.valid?.should be true
+  end
+
+  it "should fail to generate a QuirkDetail with a blank comment" do
+    q = Factory.build(:quirk)
+    s = Factory.build(:server)
+    qd = QuirkDetail.new(:quirk_id=>q.id, :server_id=>s.id, :comment => '')
+    q.valid?.should be true
+    s.valid?.should be true
+    qd.valid?.should be false
+  end
 end
