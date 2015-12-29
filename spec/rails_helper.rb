9606f3d5459b3a82a069b03c24edef0e2b0ae85b
Bert Driehuis <bert.driehuis@cyso.nl>
2015-12-29 14:53:48 +0100

    Silence Test::Unit (https://github.com/rspec/rspec-rails/issues/1171)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,3 +44,6 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 end
+
+# prevent Test::Unit's AutoRunner from executing during RSpec's rake task
+Test::Unit.run = true if defined?(Test::Unit) && Test::Unit.respond_to?(:run=)
