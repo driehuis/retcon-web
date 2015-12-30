6e659ed5cf63f524be9274385e9ed9be7f5f03cb
Bert Driehuis <driehuis@playbeing.org>
2015-12-30 18:27:03 +0100

    Make the search-as-we-type AJAX code reload the data if we already have a value on page load. This makes the back button work as expected.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,20 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+var outstanding = 0;
 $(document).ready(function() {
   $('input#search_hostname_cont').quicksearch('#server_list ol li', {delay: 100});
   $(".sortable").tablesorter();
 
-  var outstanding = 0;
+  if (($("#search_hostname_cont")).val().length > 0) {
+    get_ajax_content($("#search_hostname_cont"))
+  }
   $("#search_hostname_cont").bind("keyup", function() {
-    outstanding++;
-    var url = $(this).attr("action"); // grab the URL from the form's action value.
-    var formData = $(this).serialize(); // grab the data in the form
-    var jqxhr = $.get(url, formData, function(html) { // perform an AJAX get, the trailing function is what happens on successful get.
-      if (outstanding == jqxhr.myGeneration) {
-        $("#listing").html(html); // replace the "results" div with the result of action taken
-      }
-    });
-    jqxhr.myGeneration = outstanding;
+    get_ajax_content(this)
   });
 
   $(".toggle").live('click', function(event){
@@ -37,4 +32,16 @@ function add_fields(link, association, content) {
         var new_id = new Date().getTime();
         var regexp = new RegExp("new_" + association, "g");
         $(link).parent().before(content.replace(regexp, new_id));
+}
+
+function get_ajax_content(field) {
+    outstanding++;
+    var url = $(field).attr("action"); // grab the URL from the form's action value.
+    var formData = $(field).serialize(); // grab the data in the form
+    var jqxhr = $.get(url, formData, function(html) { // perform an AJAX get, the trailing function is what happens on successful get.
+      if (outstanding == jqxhr.myGeneration) {
+        $("#listing").html(html); // replace the "results" div with the result of action taken
+      }
+    });
+    jqxhr.myGeneration = outstanding;
 }
