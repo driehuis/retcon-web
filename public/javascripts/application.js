2bfd16e7c8f605045f411e0f51379a2af624136f
retcon <systeembeheer@cyso.nl>
2015-12-30 18:23:10 +0100

    Make the search-as-we-type AJAX code reload the data if we already have a value on page load. This makes the back button work as expected.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,22 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+var outstanding = 0;
 $(document).ready(function() {
   $('input#server_search').quicksearch('#server_list ol li', {delay: 100});
   $(".sortable").tablesorter();
-  
-  var outstanding = 0;
-  $("#search").bind("keyup", function() {
-    outstanding++;
-    var url = $(this).attr("action"); // grab the URL from the form's action value.
-    var formData = $(this).serialize(); // grab the data in the form
-    var jqxhr = $.get(url, formData, function(html) { // perform an AJAX get, the trailing function is what happens on successful get.
-      if (outstanding == jqxhr.myGeneration) {
-        $("#listing").html(html); // replace the "results" div with the result of action taken
-      }
-    });
-    jqxhr.myGeneration = outstanding;
+
+  var s = $("#search_hostname_like");
+  if (s && s.val() && s.val().length > 0) {
+    get_ajax_content(s)
+  }
+  $("#search_hostname_like").bind("keyup", function() {
+    get_ajax_content(this)
   });
-  
+
   $(".toggle").live('click', function(event){
     //alert('.toggle ' + event.target.nodeName);
     if (event.target.nodeName != 'P') { // ignore clicks in paragraphs to allow copy&paste
@@ -37,4 +33,16 @@ function add_fields(link, association, content) {
         var new_id = new Date().getTime();
         var regexp = new RegExp("new_" + association, "g");
         $(link).parent().before(content.replace(regexp, new_id));
+}
+
+function get_ajax_content(field) {
+    outstanding++;
+    var url = $(field).attr("action"); // grab the URL from the form's action value.
+    var formData = $(field).serialize(); // grab the data in the form
+    var jqxhr = $.get(url, formData, function(html) { // perform an AJAX get, the trailing function is what happens on successful get.
+      if (outstanding == jqxhr.myGeneration) {
+        $("#listing").html(html); // replace the "results" div with the result of action taken
+      }
+    });
+    jqxhr.myGeneration = outstanding;
 }
